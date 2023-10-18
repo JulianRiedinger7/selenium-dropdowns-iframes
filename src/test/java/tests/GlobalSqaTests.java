a4420ebb2dd931c7e20723f93159fa5cbb3482f4
Julian Riedinger <julianriedinger27@gmail.com>
2023-10-18 20:07:35 -0300

    removed unused imports

--- src/test/java/tests/GlobalSqaTests.java
+++ src/test/java/tests/GlobalSqaTests.java
@@ -2,17 +2,14 @@ package tests;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
-import java.util.Set;
 
 public class GlobalSqaTests {
 
